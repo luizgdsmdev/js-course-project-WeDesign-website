360c9038b1cbd58332b8e0e1d823c625b7fb9afe
Luiz Messias <luizgdsmdev@gmail.com>
2025-08-04 12:44:37 -0300

    Adding mobile interaction for navgation

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+
+
+//Deal with menu animation
+let nav__menuTop = document.getElementById("nav__menu-top");
+window.addEventListener("scroll", () => {
+    console.log(this.scrollY);
+    this.scrollY > 40 ? nav__menuTop.style.backgroundColor = "rgba(42, 45, 56, 0.5)" : nav__menuTop.style.backgroundColor = "rgba(42, 45, 56, 1)";
+});
+
+//Mobile menu handler
+let iconMenuHamburguer = document.getElementById("menu__logo-hamburguer");
+iconMenuHamburguer.onclick = () =>{
+    //Menu list wrapper
+    document.getElementById("nav__itens-wrapper").classList.toggle("showMobilemenu");
+
+    iconMenuHamburguer.classList.toggle("changeIcon");
+    iconMenuHamburguer.classList.toggle("changeIconOut");
+    console.log(iconMenuHamburguer.src.includes("hamburguer.svg"))
+    setTimeout(() => {
+        iconMenuHamburguer.src.includes("hamburguer.svg") ? iconMenuHamburguer.src = "./assets/images/menu/svgexport-71.svg" : iconMenuHamburguer.src = "./assets/images/menu/hamburguer.svg"
+    }, 1600);
+};
